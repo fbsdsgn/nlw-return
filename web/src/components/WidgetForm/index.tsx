207055cdbb01148fbaf8dc3b699f8135730ce518
Felipe B Souza <fbsdsgn@gmail.com>
2022-05-09 13:29:58 -0300

    project nlw return

---
+++ web/src/components/WidgetForm/index.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+import bugImageUrl from "../../assets/bug.svg";
+import ideaImageUrl from "../../assets/idea.svg";
+import thougthImageUrl from "../../assets/thought.svg";
+
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSucessStep } from "./Steps/FeedbackSucessStep";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+
+export const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "Image de um inseto",
+    },
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImageUrl,
+      alt: "Image de uma lâmpada",
+    },
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thougthImageUrl,
+      alt: "Imagem de uma nuvem de pensamento",
+    },
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
+  function handleRestartFeedback() {
+    setFeedbackSent(false)
+    setFeedbackType(null);
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {feedbackSent ? (
+        <FeedbackSucessStep onFeedbackRestartRequested={handleRestartFeedback}/>
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              onFeedbackRestartRequested={handleRestartFeedback}
+              feedbackType={feedbackType}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      )}
+      <footer className="text-xs text-neutral-400">
+        Feito com ♥ pela{" "}
+        <a
+          className="underline underline-offset-2"
+          href="https://rocketseat.com.br"
+        >
+          Rocketseat
+        </a>
+      </footer>
+    </div>
+  );
+}
